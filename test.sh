5f3c0f04ae77588b8bb79edca77df6bfb29d2cfb
Willem <w.meijer.5@student.rug.nl>
2024-03-07 10:56:29 +0000

    rm ipython

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 
 python3 -m reference_cleaner\
     --project-dir ./data/icse_2025\
-    --whitelist ./data/icse_2025/field_whitelist.txt\
+    --whitelist ./data/field_whitelist.txt\
     --output-file ./data/icse_2025_bibliography.bib
     
